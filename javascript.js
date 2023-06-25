dd310aa55cb98af01c43b2a1f2de744b1a98fa74
Felipe-12345 <130808325+Felipe-12345@users.noreply.github.com>
2023-06-25 17:08:40 -0300

    javascript

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function clicar(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/A1.jpg')"
+}
+
+function clicar2(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/A2.jpeg')"
+}
+
+function clicar3(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/A3.jpeg')"
+    ar = document.querySelector('.ar')
+    ar.style.backgroundPosition = 'center center'
+}
+
+function clicar4(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/A4.jpeg')"
+}
+
+function clicar5(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/A5.jpeg')"
+}
+
