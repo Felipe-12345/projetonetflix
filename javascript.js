87c43e2f6a667c465aa99da9c176ff1ce66bcaef
Felipe-12345 <130808325+Felipe-12345@users.noreply.github.com>
2023-06-25 17:42:54 -0300

    js att

--- javascript.js
+++ javascript.js
@@ -25,3 +25,40 @@ function clicar5(){
     img.style.backgroundImage = "url('fotos/A5.JPEG')"
 }
 
+
+
+
+
+
+
+
+
+
+
+function clicara1(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/foto\ 2.jpeg')"
+}
+
+function clicara2(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/foto\ 3.jpg')"
+}
+
+function clicara3(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/foto\ 4.JPEG')"
+    ar = document.querySelector('.ar')
+    ar.style.backgroundPosition = 'center center'
+}
+
+function clicara4(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/foto\ 5.jpg')"
+}
+
+function clicara5(){
+    img = document.querySelector('.ar')
+    img.style.backgroundImage = "url('fotos/duda\ 1.jpg')"
+}
+
